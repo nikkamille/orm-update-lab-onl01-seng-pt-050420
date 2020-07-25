e354a7a418dcb072ae79f5a6a4f2b70c73a0cd08
nikkamille <k.pagibigan@gmail.com>
2020-07-25 16:34:42 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -21,10 +21,63 @@ class Student
         grade TEXT
         )
     SQL
+    
     DB[:conn].execute(sql) 
+
   end
 
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
 
+    DB[:conn].execute(sql)
+
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)  
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
+
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+
+  end
 
 
 end
